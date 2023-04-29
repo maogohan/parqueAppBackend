b29d0461e76940771f8059b2d8bc8f465d13618d
Mauricio Ruiz Beltran <mruiz.sistemas@gmail.com>
2023-04-29 11:24:18 -0500

    fixed the vehicle rest and controller

--- src/main/java/com/parqueApp/parqueApp/repository/VehicleRepository.java
+++ src/main/java/com/parqueApp/parqueApp/repository/VehicleRepository.java
@@ -18,7 +18,6 @@ package com.parqueApp.parqueApp.repository;
 import com.parqueApp.parqueApp.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/main/java/com/parqueApp/parqueApp/rest/VehicleRest.java
+++ src/main/java/com/parqueApp/parqueApp/rest/VehicleRest.java
@@ -19,7 +19,6 @@ import com.parqueApp.parqueApp.model.Vehicle;
 import com.parqueApp.parqueApp.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
